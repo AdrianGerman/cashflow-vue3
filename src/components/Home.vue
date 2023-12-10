8c27d419e434b47b31b891e055b145461464725f
AdrianGerman <adrian.german1019@gmail.com>
2023-12-10 17:03:55 -0600

    header and content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>Hola desde el fin del mundo</div>
+  <Layout>
+    <template #header>
+      <Header></Header>
+    </template>
+  </Layout>
 </template>
+
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+
+export default {
+  components: {
+    Layout,
+    Header,
+  },
+};
+</script>
