ed070caaf758aeb4a74166f7ac808d2cbda2fe2d
AdrianGerman <adrian.german1019@gmail.com>
2023-12-10 16:16:16 -0600

    splashscreen

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,48 @@
 <template>
   <div>
-    <Home />
+    <Suspense>
+      <template #default>
+        <Home />
+      </template>
+      <template #fallback>
+        <SplashScreen />
+      </template>
+    </Suspense>
   </div>
 </template>
 
 <script>
-import Home from "@/components/Home.vue";
+import SplashScreen from "./components/SplashScreen.vue";
+import { defineAsyncComponent } from "vue";
 
 export default {
   components: {
-    Home,
+    SplashScreen,
+    Home: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import("./components/Home.vue"));
+          }, 2500);
+        })
+    ),
   },
 };
 </script>
 
 <style>
-body {
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
   background: rgb(13, 17, 23);
   color: aliceblue;
+}
+
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
 }
 </style>
